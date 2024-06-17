a3cc32c3eaed73afb5af07d76ece392a783c6ecf
MuriloDSantos <49132949+MuriloNSantos17@users.noreply.github.com>
2024-06-17 09:05:39 -0300

    Commit start method

--- 01-FUNDAMENTOS-NODEJS/src/server.js
+++ 01-FUNDAMENTOS-NODEJS/src/server.js
@@ -1,8 +1,26 @@
 import http from 'node:http';
 
+const users = [];
+
 const server = http.createServer((request, response) => {
     console.log('server is running...')
-    return response.end('Hello World...')
+    const { method, url } = request;
+    console.log(method, url)
+
+    if (method === 'GET' && url == '/users') {
+        return response.end(JSON.stringify(users))
+    }
+
+    if (method == 'POST' && url == '/users') {
+        users.push({
+            name: 'John doe',
+            email: 'johndoe@example.com',
+            id: 1,
+        })
+        return response.writeHead(201).end()
+    }
+
+    return response.writeHead(404).end('Not found')
 })
 
 server.listen(3333)
