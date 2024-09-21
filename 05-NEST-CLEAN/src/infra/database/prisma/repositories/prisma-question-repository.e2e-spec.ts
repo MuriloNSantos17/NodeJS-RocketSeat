c6e71f8127a7a192c66646920a82fa0106b534fa
MuriloDSantos <49132949+MuriloNSantos17@users.noreply.github.com>
2024-09-21 13:52:41 -0300

    Commit final test

--- 05-NEST-CLEAN/src/infra/database/prisma/repositories/prisma-question-repository.e2e-spec.ts
+++ 05-NEST-CLEAN/src/infra/database/prisma/repositories/prisma-question-repository.e2e-spec.ts
@@ -8,8 +8,8 @@ import { Test } from '@nestjs/testing'
 import { AttachmentFactory } from 'test/factories/make-attachment'
 import { QuestionFactory } from 'test/factories/make-question'
 import { QuestionAttachmentFactory } from 'test/factories/make-question-attachment'
-
 import { StudentFactory } from 'test/factories/make-student'
+
 describe('Prisma Questions Repository (E2E)', () => {
     let app: INestApplication
     let studentFactory: StudentFactory
@@ -18,6 +18,7 @@ describe('Prisma Questions Repository (E2E)', () => {
     let questionAttachmentFactory: QuestionAttachmentFactory
     let cacheRepository: CacheRepository
     let questionsRepository: QuestionsRepository
+
     beforeAll(async () => {
         const moduleRef = await Test.createTestingModule({
             imports: [AppModule, DatabaseModule, CacheModule],
@@ -28,65 +29,93 @@ describe('Prisma Questions Repository (E2E)', () => {
                 QuestionAttachmentFactory,
             ],
         }).compile()
+
         app = moduleRef.createNestApplication()
+
         studentFactory = moduleRef.get(StudentFactory)
         questionFactory = moduleRef.get(QuestionFactory)
         attachmentFactory = moduleRef.get(AttachmentFactory)
         questionAttachmentFactory = moduleRef.get(QuestionAttachmentFactory)
         cacheRepository = moduleRef.get(CacheRepository)
         questionsRepository = moduleRef.get(QuestionsRepository)
+
         await app.init()
     })
+
     it('should cache question details', async () => {
         const user = await studentFactory.makePrismaStudent()
+
         const question = await questionFactory.makePrismaQuestion({
             authorId: user.id,
         })
+
         const attachment = await attachmentFactory.makePrismaAttachment()
+
         await questionAttachmentFactory.makePrismaQuestionAttachment({
             attachmentId: attachment.id,
             questionId: question.id,
         })
+
         const slug = question.slug.value
+
         const questionDetails = await questionsRepository.findDetailsBySlug(slug)
+
         const cached = await cacheRepository.get(`question:${slug}:details`)
+
         expect(cached).toEqual(JSON.stringify(questionDetails))
     })
+
     it('should return cached question details on subsequent calls', async () => {
         const user = await studentFactory.makePrismaStudent()
+
         const question = await questionFactory.makePrismaQuestion({
             authorId: user.id,
         })
+
         const attachment = await attachmentFactory.makePrismaAttachment()
+
         await questionAttachmentFactory.makePrismaQuestionAttachment({
             attachmentId: attachment.id,
             questionId: question.id,
         })
+
         const slug = question.slug.value
+
         await cacheRepository.set(
             `question:${slug}:details`,
             JSON.stringify({ empty: true }),
         )
+
         const questionDetails = await questionsRepository.findDetailsBySlug(slug)
+
         expect(questionDetails).toEqual({ empty: true })
     })
+
     it('should reset question details cache when saving the question', async () => {
         const user = await studentFactory.makePrismaStudent()
+
         const question = await questionFactory.makePrismaQuestion({
             authorId: user.id,
         })
+
         const attachment = await attachmentFactory.makePrismaAttachment()
+
         await questionAttachmentFactory.makePrismaQuestionAttachment({
             attachmentId: attachment.id,
             questionId: question.id,
         })
+
         const slug = question.slug.value
+
         await cacheRepository.set(
             `question:${slug}:details`,
             JSON.stringify({ empty: true }),
         )
+
         await questionsRepository.save(question)
+
         const cached = await cacheRepository.get(`question:${slug}:details`)
+
         expect(cached).toBeNull()
     })
 })
