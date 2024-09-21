7fad188337f23225b5024852751b92c04783c8c9
MuriloDSantos <49132949+MuriloNSantos17@users.noreply.github.com>
2024-09-21 13:33:43 -0300

    Commit start cache

--- 05-NEST-CLEAN/src/infra/http/http.module.ts
+++ 05-NEST-CLEAN/src/infra/http/http.module.ts
@@ -41,6 +41,8 @@ import { FetchAnswerCommentsController } from '@/domain/forum/application/use-ca
 import { UploadAttachmentController } from './controllers/upload-attachment.controller'
 import { StorageModule } from '../storage/storage.module'
 import { UploadAndCreateAttachmentUseCase } from '@/domain/forum/application/use-cases/upload-and-create-attachment'
+import { ReadNotificationController } from './controllers/read-notification.controller'
+import { ReadNotificationUseCase } from '@/domain/notification/application/use-cases/read-notification'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule, StorageModule],
@@ -64,6 +66,7 @@ import { UploadAndCreateAttachmentUseCase } from '@/domain/forum/application/use
     FetchQuestionCommentsController,
     FetchAnswerCommentsController,
     UploadAttachmentController,
+    ReadNotificationController
   ],
   providers: [
     CreateQuestionUseCase,
@@ -84,7 +87,8 @@ import { UploadAndCreateAttachmentUseCase } from '@/domain/forum/application/use
     DeleteAnswerCommentUseCase,
     FetchQuestionCommentsUseCase,
     FetchAnswerCommentsUseCase,
-    UploadAndCreateAttachmentUseCase
+    UploadAndCreateAttachmentUseCase,
+    ReadNotificationUseCase
   ],
 })
 export class HttpModule { }
